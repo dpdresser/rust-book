fd7e3c98e6a3080376284268a5f3d45046654788
DESKTOP-M84ENAA\Dan <ddresser05@gmail.com>
2024-08-14 15:27:40 -0500

    Unsafe function example

--- chp19-1_unsafe_rust/src/main.rs
+++ chp19-1_unsafe_rust/src/main.rs
@@ -9,4 +9,13 @@ fn main() {
         println!("r1 is: {}", *r1);
         println!("r2 is: {}", *r2);
     }
+
+    // Unsafe functions
+    unsafe fn dangerous() {}
+
+    // dangerous(); -> compile error
+
+    unsafe {
+        dangerous();
+    }
 }
