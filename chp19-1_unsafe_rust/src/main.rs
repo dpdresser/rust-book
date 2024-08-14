c4dd779c54d32cd45a7c767a2937908ac01ede30
DESKTOP-M84ENAA\Dan <ddresser05@gmail.com>
2024-08-14 15:22:26 -0500

    Example of mutable and immutable raw pointer being accessed in unsafe block

--- chp19-1_unsafe_rust/src/main.rs
+++ chp19-1_unsafe_rust/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 5;
+
+    // Can create raw pointers in safe code; but can't dereference raw pointers outside of an unsafe block
+    let r1 = &num as *const i32; // immutable raw pointer
+    let r2 = &mut num as *mut i32; // mutable raw pointer
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
 }
