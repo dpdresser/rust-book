499f5a18515855b31cf13752028afc20634a229f
DESKTOP-M84ENAA\Dan <ddresser05@gmail.com>
2024-08-21 08:40:27 -0500

    Overriding a default type parameter when implementing a Trait

--- chp19-2_advanced_traits/src/main.rs
+++ chp19-2_advanced_traits/src/main.rs
@@ -17,10 +17,29 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Millimeters(u32);
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters { // Sets the value for the Rhs type parameter instead of using default Self
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3}
+    );
+
+    assert_eq!(
+        Millimeters(5250),
+        Millimeters(250) + Meters(5)
     );
 }
 
