dde1d29b0524b10b00fc66cc2def1afc6f1ad073
DESKTOP-M84ENAA\Dan <ddresser05@gmail.com>
2024-08-21 11:02:21 -0500

    Function as argument to map

--- chp19-4_advanced_functions_and_closures/src/main.rs
+++ chp19-4_advanced_functions_and_closures/src/main.rs
@@ -8,7 +8,16 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 { // Specifiy parameter f is an 
     f(arg) + f(arg)
 }
 
+#[derive(Debug)]
+enum Status {
+    Value(u32),
+    Stop,
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
     println!("The answer is: {answer}");
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect(); // Name of each enum variant also becomes initializer function
+    println!("{:?}", list_of_statuses);
 }
