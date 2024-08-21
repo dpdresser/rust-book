e62f485f5935ef0b01845e4b8069a9ea2bfb4ecf
DESKTOP-M84ENAA\Dan <ddresser05@gmail.com>
2024-08-21 10:59:30 -0500

    Function pointer example

--- chp19-4_advanced_functions_and_closures/src/main.rs
+++ chp19-4_advanced_functions_and_closures/src/main.rs
@@ -1,3 +1,14 @@
+// Function Pointer example
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 { // Specifiy parameter f is an fn that takes one parameter of type i32 and returns an i32
+    f(arg) + f(arg)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer = do_twice(add_one, 5);
+    println!("The answer is: {answer}");
 }
